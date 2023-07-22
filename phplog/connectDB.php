c8dee00d2aa89e5a53796a50c3fcabedcf4f5aa9
“al <alspok@gmail.com>
2023-07-22 09:35:40 +0000

    .

---
+++ phplog/connectDB.php
@@ -0,0 +1,16 @@
+<?php
+
+$server = "db-mysql-fra1-42194-do-user-14106707-0.b.db.ondigitalocean.com";
+$username = "doadmin";
+$password = "AVNS_IzETuacH57EOU-TThcJ";
+$database = "e_deals_db";
+$port = 25060;
+$sslmode = "REQUIRE";
+
+$conn = mysqli_connect($server, $username, $password, $database, $port, $sslmode);
+
+if (!$conn){
+    die("<br>Connection failed: " . mysqli_connect_error());
+  }
+
+?>

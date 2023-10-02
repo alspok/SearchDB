fddfe2ec9e68b106b590a8d5fa8ca20a2a97d0c9
“al <alspok@gmail.com>
2023-10-02 10:03:23 +0000

    .

--- phplog/exportdb.php
+++ phplog/exportdb.php
@@ -25,7 +25,7 @@
                                 <!-- <input type="text" id ="user" name="export_for" />   -->
                             </p>  
                             <p>  
-                                <label class='label-fnt'> File name  <span class="sfnt">( extention *.export.csv )</span> </label><br>  
+                                <label class='label-fnt'> File name  <span class="sfnt">(*.export.csv )</span> </label><br>  
                                 <input class='sfnt' type="text" name="file_name" />  
                             </p>  
                             <p>  
@@ -58,107 +58,106 @@
                             </p>  
                     </form>
                 </div>
+                
                 <div class='col-auto sfnt border border-2'>
-                <h6>Exported files</h6>
-                    <?php
-                    $files = scandir("../ExportFiles");
-                    foreach($files as $file){
-                        if($file == '.' or $file == '..' or $file == '.gitkeep'){
-                            continue;
+                    <h6>Exported files</h6>
+                        <?php
+                        $files = scandir("../ExportFiles");
+                        foreach($files as $file){
+                            if($file == '.' or $file == '..' or $file == '.gitkeep'){
+                                continue;
+                            }
+                            else{
+                                $file_name = "../ExportFiles/" . $file;
+                                $cdate = date ("Y-m-d H:i:s.", filemtime($file_name));
+                                // print("<a class='.fnt'>{$file}.export.csv created at: {$cdate}</a></br>");
+                                print("<a class='sfnt'><b>{$file}.export.csv</b> created at: {$cdate}</a></br>");
+                            }
                         }
-                        else{
-                            $file_name = "../ExportFiles/" . $file;
-                            $cdate = date ("Y-m-d H:i:s.", filemtime($file_name));
-                            // print("<a class='.fnt'>{$file}.export.csv created at: {$cdate}</a></br>");
-                            print("<a class='sfnt'><b>{$file}.export.csv</b> created at: {$cdate}</a></br>");
-                        }
-                    }
-                    print("<br><br>");
-                    include_once("../Classes/ConnectDB.php");
-                    $connection = new ConncctDB();
-                    $conn = $connection->connectDB();
+                        print("<br><br>");
+                        include_once("../Classes/ConnectDB.php");
+                        $connection = new ConncctDB();
+                        $conn = $connection->connectDB();
 
-                    $query = "select * from e_supplier_tbl";
-                    $result = $conn -> query($query);
+                        $query = "select * from e_supplier_tbl";
+                        $result = $conn -> query($query);
 
-                    print("<table class='h-auto d-inline-block w-auto p-3'>");
-                    print("<tr>");
-                    while ($field_info = $result->fetch_field()){
-                        print("<th class='sfnt'>{$field_info->name}</th>");
-                    }
-                    print("</tr>");
-                    $suplier_array = array();
-                    while($row = $result->fetch_row()){
+                        print("<table class='h-auto d-inline-block w-auto p-3'>");
                         print("<tr>");
-                        foreach($row as $item){
-                            print("<td class='sfnt'>{$item}</td>");
+                        while ($field_info = $result->fetch_field()){
+                            print("<th class='sfnt'>{$field_info->name}</th>");
                         }
                         print("</tr>");
-                    }
-                    print("</table>");
+                        $suplier_array = array();
+                        while($row = $result->fetch_row()){
+                            print("<tr>");
+                            foreach($row as $item){
+                                print("<td class='sfnt'>{$item}</td>");
+                            }
+                            print("</tr>");
+                        }
+                        print("</table>");
 
-                    $result->free_result();
-                    $connection->closeDB();
-                    print("<br><br>");
-                    ?>
+                        $result->free_result();
+                        $connection->closeDB();
+
+                        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                            $export_for = $_POST['export_for'];
+                            if($export_for == NULL){
+                                $export_for_bool = FALSE;
+                                print("<div class='csfnt'><b>'Export for'</b> is empty</div>");
+                            }
+                            else{
+                                $export_for_bool = TRUE;
+                            }
+                            $file_name = $_POST['file_name'];
+                            if($file_name == NULL){
+                                $file_name_bool = FALSE;
+                                print("<div class='csfnt'><b>'File name'</b> field is empty</div>");
+                            }
+                            else{
+                                $file_name_bool = TRUE;
+                            }
+                            $margin = $_POST['margin'];
+                            if($margin == NULL){
+                                $margin_bool = FALSE;
+                                print("<div class='csfnt'><b>'Margin'</b> field is empty</div>");
+                            }
+                            else{
+                                $margin_bool = TRUE;
+                            }
+                            $min_stock = $_POST['min_stock'];
+                            if($min_stock == NULL){
+                                $min_stock_bool = FALSE;
+                                print("<div class='csfnt'><b>'Min stock'</b> field is empty</div>");
+                            }
+                            else{
+                                $min_stock_bool = TRUE;
+                            }
+                            $min_price = $_POST['min_price'];
+                            if($min_price == NULL){
+                                $min_price_bool = FALSE;
+                                print("<div class='csfnt'><b>'Min price'</b> field is empty</div>");
+                            }
+                            else{
+                                $min_price_bool = TRUE;
+                            }
+                            $companies = $_POST['companies'];
+                            if($companies == NULL){
+                                $companies_bool = FALSE;
+                                print("<div class='csfnt'><b>'Suppliers'</b> field is empty</div>");
+                            }
+                            else{
+                                $companies_bool = TRUE;
+                            }
+                        }
+                        ?>
                 </div>
             </div>
-            <!-- <div class="row d-flex align-items-center justify-content-left"> -->
+
             <div class='row' style=width: 100%>
                 <div class='col-auto sfnt border border-2'>
                     <?php
-                    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                        $export_for = $_POST['export_for'];
-                        if($export_for == NULL){
-                            $export_for_bool = FALSE;
-                            print("<div class='csfnt'><b>'Export for'</b> is empty</div>");
-                        }
-                        else{
-                            $export_for_bool = TRUE;
-                        }
-                        $file_name = $_POST['file_name'];
-                        if($file_name == NULL){
-                            $file_name_bool = FALSE;
-                            print("<div class='csfnt'><b>'File name'</b> field is empty</div>");
-                        }
-                        else{
-                            $file_name_bool = TRUE;
-                        }
-                        $margin = $_POST['margin'];
-                        if($margin == NULL){
-                            $margin_bool = FALSE;
-                            print("<div class='csfnt'><b>'Margin'</b> field is empty</div>");
-                        }
-                        else{
-                            $margin_bool = TRUE;
-                        }
-                        $min_stock = $_POST['min_stock'];
-                        if($min_stock == NULL){
-                            $min_stock_bool = FALSE;
-                            print("<div class='csfnt'><b>'Min stock'</b> field is empty</div>");
-                        }
-                        else{
-                            $min_stock_bool = TRUE;
-                        }
-                        $min_price = $_POST['min_price'];
-                        if($min_price == NULL){
-                            $min_price_bool = FALSE;
-                            print("<div class='csfnt'><b>'Min price'</b> field is empty</div>");
-                        }
-                        else{
-                            $min_price_bool = TRUE;
-                        }
-                        $companies = $_POST['companies'];
-                        if($companies == NULL){
-                            $companies_bool = FALSE;
-                            print("<div class='csfnt'><b>'Suppliers'</b> field is empty</div>");
-                        }
-                        else{
-                            $companies_bool = TRUE;
-                        }
-                        print("<br>");
-                    }
-
                     include_once("../Classes/ConnectDB.php");
                     $connection = new ConncctDB();
                     $conn = $connection->connectDB();
@@ -166,27 +165,37 @@
                     $query = "select * from e_deals_tbl";
                     $result = $conn -> query($query);
 
-                    print("<table class='h-auto d-inline-block w-auto p-3'>");
-                    print("<tr>");
-                    while ($field_info = $result->fetch_field()){
-                        print("<th class='ssfnt'>{$field_info->name}</th>");
-                    }
-                    print("</tr>");
+                    if($export_for_bool){
+                        $file_name = "../ExportFiles/{$export_for}/{$export_for}.export.csv";
+                        var_dump($export_for_bool);
+                        var_dump($file_name);
+                        $fhw = fopen($file_name, "w");
 
-                    while($row = $result->fetch_row()){
-                        // var_dump("<a>{$row[1]}</a><br>}");
-                        if(in_array($row[1], $companies)){
-                            print("<tr>");
-                            foreach($row as $item){
-                                print("<td class='ssfnt'>{$item}</td>");
-                            }
-                            print("</tr>");
+                        print("<table class='h-auto d-inline-block w-auto p-3'>");
+                        print("<tr>");
+                        while ($field_info = $result->fetch_field()){
+                            print("<th class='ssfnt'>{$field_info->name}</th>");
                         }
-                    }
-                    print("</table>");
+                        print("</tr>");
 
-                    $result->free_result();
-                    $connection->closeDB();
+                        while($row = $result->fetch_row()){
+                            // var_dump("<a>{$row[1]}</a><br>}");
+                            if(in_array($row[1], $companies)){
+                                print("<tr>");
+                                foreach($row as $item){
+                                    print("<td class='ssfnt'>{$item}</td>");
+                                }
+                                print("</tr>");
+                            }
+                        }
+                        print("</table>");
+
+                        $result->free_result();
+                        $connection->closeDB();
+                    }
+                    else{
+                        print("<div class='csfnt'>Enter export file name</div>");
+                    }
                     ?>
                 </div>
             </div>
